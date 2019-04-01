e11ca2e0cf626d5a3289df7f04896821c3f169d8
Bruno <b.hidalgo@tcs.com>
2019-03-31 21:40:05 -0300

    created services and components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRecipeComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    IngredientsListComponent
   ],
   imports: [
     BrowserModule
