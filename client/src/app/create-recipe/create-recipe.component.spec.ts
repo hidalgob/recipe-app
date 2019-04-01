3b34bea809f062a22f66f1c512e8ea5254790d53
Bruno <b.hidalgo@tcs.com>
2019-03-31 23:10:35 -0300

    configured create-recipe component

--- client/src/app/create-recipe/create-recipe.component.spec.ts
+++ client/src/app/create-recipe/create-recipe.component.spec.ts
@@ -5,20 +5,20 @@ import { CreateRecipeComponent } from './create-recipe.component';
 describe('CreateRecipeComponent', () => {
   let component: CreateRecipeComponent;
   let fixture: ComponentFixture<CreateRecipeComponent>;
-
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateRecipeComponent ]
     })
     .compileComponents();
   }));
-
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateRecipeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- client/src/app/create-recipe/create-recipe.component.ts
+++ client/src/app/create-recipe/create-recipe.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe';
+import { Ingredients } from "../ingredients";
+import { RecipeService } from '../recipe.service';
+import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'create-recipe',
@@ -6,10 +12,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-recipe.component.css']
 })
 export class CreateRecipeComponent implements OnInit {
+  
+  recipe: Recipe = new Recipe();
+  ingredients: Observable<Ingredients[]>;
+  
+  submitted = false;
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
 
-  constructor() { }
+  form: FormGroup;
 
+  constructor(private recipeService: RecipeService, private formBuilder: FormBuilder) { }
+  
   ngOnInit() {
-  }
+    this.recipeService.getIngredientsList()
+    .subscribe(ingredients => {
+      this.dropdownList = ingredients as Ingredients[];
+    })
 
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      enableCheckAll: false,
+      selectAllText: 'Selecionar Tudo',
+      unSelectAllText: 'Desselecionar Tudo',
+      itemsShowLimit: 10,
+      allowSearchFilter: true,
+      searchPlaceholderText: 'Pesquisar',
+    };
+
+    this.form = this.formBuilder.group({
+      name: [null,  [Validators.required, Validators.minLength(1)]],
+      servings: [null, [Validators.required, Validators.minLength(1)]],
+      calories: [null, [Validators.required, Validators.minLength(1)]],
+      ingredients: [null, [Validators.required]],
+      howTo: [null,  [Validators.required, Validators.minLength(1)]],
+    });
+    
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  newRecipe(): void {
+    this.submitted = false;
+    this.recipe = new Recipe();
+  }
+  save() {
+    this.recipeService.createRecipe(this.recipe)
+    .subscribe(data => console.log(data), error => console.log(error));
+    this.recipe = new Recipe();
+  }
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.selectedItems)
+    this.save();
+  }
 }
