4496aa9806bf58ce9a1b99fe20f0b06f00eadafd
Bruno <b.hidalgo@tcs.com>
2019-03-31 22:51:44 -0300

    created repositories

--- server/src/main/java/com/opus/recipeapp/springbootrecipeapp/repository/IngredientsRepository.java
+++ server/src/main/java/com/opus/recipeapp/springbootrecipeapp/repository/IngredientsRepository.java
@@ -1,5 +1,13 @@
 package com.opus.recipeapp.springbootrecipeapp.repository;
 
-public class IngredientsRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.opus.recipeapp.springbootrecipeapp.model.Ingredients;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface IngredientsRepository extends CrudRepository<Ingredients, Long> {
+	List<Ingredients> findAllByOrderByName();
 }
--- server/src/main/java/com/opus/recipeapp/springbootrecipeapp/repository/RecipeRepository.java
+++ server/src/main/java/com/opus/recipeapp/springbootrecipeapp/repository/RecipeRepository.java
@@ -1,5 +1,18 @@
 package com.opus.recipeapp.springbootrecipeapp.repository;
 
-public class RecipeRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.opus.recipeapp.springbootrecipeapp.model.Recipe;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+  List<Recipe> findByName(String name);
+  List<Recipe> findByIngredientsId(long id);
+  
+  @Query("select recipe.ingredients from Recipe recipe")
+  Iterable<Recipe> findIngredientsById(long id);
 }
