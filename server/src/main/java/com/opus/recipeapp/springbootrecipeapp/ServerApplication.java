b8f8309bb648a2869ee99f2473539515b0314d61
Bruno <b.hidalgo@tcs.com>
2019-03-31 21:25:57 -0300

    initial structure for spring boot server

---
+++ server/src/main/java/com/opus/recipeapp/springbootrecipeapp/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.opus.recipeapp.springbootrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
